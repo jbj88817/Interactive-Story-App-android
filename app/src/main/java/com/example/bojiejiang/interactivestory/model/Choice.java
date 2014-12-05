cd883b048fc420c0672f3213cadd57481cc56b79
Bojie Jiang <jiangbojie@gmail.com>
2014-12-05 00:13:26 -0800

    Project update

---
+++ app/src/main/java/com/example/bojiejiang/interactivestory/model/Choice.java
@@ -0,0 +1,31 @@
+package com.example.bojiejiang.interactivestory.model;
+
+/**
+ * Created by bojiejiang on 12/4/14.
+ */
+public class Choice {
+
+    private String mText;
+    private int mNextPage;
+
+    public Choice(String text, int nextPage) {
+        mText = text;
+        mNextPage = nextPage;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+
+    public int getNextPage() {
+        return mNextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        mNextPage = nextPage;
+    }
+}
--- app/src/main/java/com/example/bojiejiang/interactivestory/ui/MainActivity.java
+++ app/src/main/java/com/example/bojiejiang/interactivestory/ui/MainActivity.java
@@ -1,17 +1,20 @@
-package com.example.bojiejiang.interactivestory;
+package com.example.bojiejiang.interactivestory.ui;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+
+import com.example.bojiejiang.interactivestory.R;
 
 
 public class MainActivity extends Activity {
 
     private EditText mNameField;
     private Button mStartButton;
+    private String mName;
 
 
     @Override
@@ -25,12 +28,23 @@ public class MainActivity extends Activity {
         mStartButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name = mNameField.getText().toString();
-                Toast.makeText(MainActivity.this, name, Toast.LENGTH_LONG).show();
-        }
+                mName = mNameField.getText().toString();
+                startStory();
+            }
         });
 
     }
 
+    private void startStory() {
+        Intent intent = new Intent(MainActivity.this, StoryActivity.class);
+        intent.putExtra(getString(R.string.key_name), mName);
+        startActivity(intent);
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mNameField.setText("");
+    }
 }
